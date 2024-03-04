af855dfabb58e877f9f428d4f0cc500675047779
bharathraj2126 <bharathraj2126@gmail.com>
2024-03-03 20:42:09 -0500

    getting data from mongo db

--- server.js
+++ server.js
@@ -1,29 +1,67 @@
-// Budget API
-const fs = require('fs');
-const express = require('express');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const budgetModel = require("./backenddata/data");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-const bodyParser = require('body-parser');
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
+app.use("/", express.static("public"));
 
-
-app.use('/', express.static('public'));
-
-  
-app.use(cors());
-var data;
-fs.readFile('data.json', 'utf8', (err, file) => {
-    data = file;
-    return;
-})
-
-
-app.get('/budget', (req, res) => {
-    res.send(data);
+app.get("/items", (req, res) => {
+  mongoose.connect("mongodb://127.0.0.1:27017/dataforbudgetchart", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected");
+    budgetModel.find({})
+      .then((data) => {
+        res.json(data);
+        console.log(data);
+        mongoose.connection.close();
+      })
+      .catch((connectionError) => {
+        console.error(connectionError);
+      });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 });
 
+app.post("/items", (req, res) => {
+    mongoose.connect("mongodb://127.0.0.1:27017/dataforbudgetchart", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to the database");
+      const newItem = new budgetModel(req.body);
+      budgetModel.create(newItem) 
+        .then((data) => {
+          res.json(data);
+          console.log(data);
+          mongoose.connection.close();
+        })
+        .catch((connectionError) => {
+          console.error(connectionError);
+          res.status(400).json({ error: 'An internal server error occurred due to validation failure' });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(400).json({ error: 'Internal error' });
+    });
+  });
+  
+
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+  console.log(`Service API displayed at http://localhost:${port}`);
 });
